2267c3ceff162ba1514cbde233d1d80b8d7cbd07
what1s1ove <zubko.2007@gmail.com>
2021-03-23 20:40:05 +0200

    t4-base-functionality: * jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,13 @@ import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   verbose: true,
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleNameMapper: {
+    '~/(.*)$': '<rootDir>/src/$1',
+  },
 };
 
 export default config;
