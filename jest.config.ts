8af74014d94059a3734342ac28606943b07fd1a7
what1s1ove <zubko.2007@gmail.com>
2021-04-04 09:59:54 +0300

    t14-milti-select-values: * jest config

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,7 @@ const config: Config.InitialOptions = {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
   }),
+  modulePathIgnorePatterns: ['build'],
 };
 
 export default config;
