eb0f7326ad95decd776b05d5d91adcbca85ab6b4
what1s1ove <zubko.2007@gmail.com>
2021-04-04 10:14:35 +0300

    t14-milti-select-values: + test utils file

---
+++ tests/utils.ts
@@ -0,0 +1,31 @@
+import { ElementName } from '~/common/enums';
+import { ChildElement } from '~/common/types';
+import { createElement } from '~/helpers';
+
+const createFormElement = (...children: HTMLElement[]): HTMLElement => {
+  return createElement(
+    ElementName.FORM,
+    {
+      name: '',
+    },
+    ...children,
+  );
+};
+
+const createLabelElement = (...children: ChildElement[]): HTMLElement => {
+  return createElement(ElementName.LABEL, {}, ...children);
+};
+
+const createOptionElement = (...options: string[]): HTMLOptionElement[] => {
+  return <HTMLOptionElement[]>options.map((opt) => {
+    return createElement(
+      ElementName.OPTION,
+      {
+        value: opt,
+      },
+      opt,
+    );
+  });
+};
+
+export { createLabelElement, createOptionElement, createFormElement };
