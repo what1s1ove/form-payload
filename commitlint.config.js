a2334e247bd4120bdfc1578511420231c41ecae4
Vladyslav Zubko <42296182+what1s1ove@users.noreply.github.com>
2023-12-03 17:10:32 +0200

    ci: add commitlint fp-34 (#37)
    * chore: update packages fp-34
    
    * chore: add commitlint fp-34
    
    * ci: add ci pr action fp-34
    
    * ci: remove notify action fp-34
    
    * chore: add nvmrc fp-34
    
    * ci: remove labeler from ci pr action fp-34

---
+++ commitlint.config.js
@@ -0,0 +1,18 @@
+import { RuleConfigSeverity } from '@commitlint/types';
+
+const PROJECT_PREFIXES = /** @type {const} */ ([`fp`, `release`]);
+
+/** @type {import('@commitlint/types').UserConfig} */
+let configuration = {
+  extends: [`@commitlint/config-conventional`],
+  parserPreset: {
+    parserOpts: {
+      issuePrefixes: PROJECT_PREFIXES.map((it) => `${it}-`),
+    },
+  },
+  rules: {
+    'references-empty': [RuleConfigSeverity.Error, `never`],
+  },
+};
+
+export default configuration;
