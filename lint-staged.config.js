b0191b7d1cfe856a719326b717b1c4f2fce0aa31
what1s1ove <zubko.2007@gmail.com>
2023-12-07 17:53:28 +0200

    docs: edit package.json info fp-58

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 /** @type {import('lint-staged').Config} */
 const config = {
-	'*': ['npm run lint:fs', 'npm run lint:editor'],
+	'*': ['npm run ci:lint:fs', 'npm run ci:lint:editor'],
 	'*.{json,md,yml,js}': 'prettier --write',
-	'*.js': ['npm run lint:js', "bash -c 'npm run lint:type'"],
+	'*.js': ['npm run ci:lint:js', "bash -c 'npm run ci:lint:type'"],
 };
 
 export default config;
