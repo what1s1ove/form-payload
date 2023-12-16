ebad6dabddbd72345ca6ba6fee66eaa77da94a2f
what1s1ove <zubko.2007@gmail.com>
2023-12-16 18:26:35 +0200

    style: add const modifier to constant explicitly fp-101

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 /** @type {import('lint-staged').Config} */
 const config = {
 	'*': ['npm run ci:lint:fs', 'npm run ci:lint:editor'],
-	'*.{json,md,yml,js}': 'prettier --write',
+	'*.{json,md,yml,js}': ['npm run ci:lint:format'],
 	'*.js': ['npm run ci:lint:js', "bash -c 'npm run ci:lint:type'"],
 };
 
--- src/libs/constants/value-as-array-custom-control-element-types.constant.js
+++ src/libs/constants/value-as-array-custom-control-element-types.constant.js
@@ -1,8 +1,8 @@
 import { ControlElementType } from '../enums/enums.js';
 
-const VALUE_AS_ARRAY_CUSTOM_CONTROL_ELEMENT_TYPES = [
+const VALUE_AS_ARRAY_CUSTOM_CONTROL_ELEMENT_TYPES = /** @type {const} */ ([
 	ControlElementType.CHECKBOX,
 	ControlElementType.FIELDSET,
-];
+]);
 
 export { VALUE_AS_ARRAY_CUSTOM_CONTROL_ELEMENT_TYPES };
